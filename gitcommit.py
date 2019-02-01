2c4c4d9ebac86aecb5fc2d48678bb30c52ac6185
Martin <martin.guthrie@docker.com>
2019-02-01 15:36:20 -0500

    test

--- gitcommit.py
+++ gitcommit.py
@@ -1,5 +1,5 @@
-prev_commit='6bac034cc3b39e2d7f1bdb12e985fe6249f7ded0'
-date='2019-02-01 15:31:19 -0500'
+prev_commit='0be4d199b96b04e35eb877ae0e98302d33f8293c'
+date='2019-02-01 15:32:25 -0500'
 branch=''
-version='commit_version'
+version='0.1.0-5-g0be4d19'
 
